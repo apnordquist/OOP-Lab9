003302bc7fca3473e73c5d7ee70ef42098d5993e
Adam Nordquist <apnordquist@gmail.com>
2024-03-22 13:48:52 -0600

    initialization

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OOP_Lab9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConnect CsharptoMySQL = new DBConnect();
+
+            CsharptoMySQL.Insert();
+            CsharptoMySQL.Update();
+            CsharptoMySQL.Delete();
+        }
+    }
+}
